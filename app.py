16e4c0a5a6cb3f074b956c5e9ffcccdeb4b2a951
barnez299 <barnez29@gmail.com>
2021-01-28 12:02:02 +0200

    initial routing and folder structure

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
 from flask import Flask
 
+
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello From Conda and Flask'
+# view all appointments
+@app.route('/appointments/')
+def appointment_list():
+    return 'Listing of all appointments we have.'
+
+
+
+# create an appointment
+@app.route('/appointments/create/', methods=['GET', 'POST'])
+def appointment_create():
+    return 'Form to create a new appointment.'
+
+
+
+# view single appointment
+@app.route('/appointments/<int:appointment_id>/')
+def appointment_detail(appointment_id):
+    return 'Detail of appointment #{}.'.format(appointment_id)
+
+
+# edit an appointment
+@app.route('/appointments/<int:appointment_id>/edit/', methods=['GET', 'POST'])
+def appointment_edit(appointment_id):
+    return 'Form to edit appointment #.'.format(appointment_id)
+
+
+
+
+# delete an appointment
+@app.route('/appointments/<int:appointment_id>/delete/', methods=['DELETE'])
+def appointment_delete(appointment_id):
+    raise NotImplementedError('DELETE')
+
+
 
 
 if __name__=='__main__':
